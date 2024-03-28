0dd62165e8676d6bc53e76443ffe8ffb6ef3f605
peppegerace <peppegerace47@gmail.com>
2024-03-28 14:45:11 +0100

    [edit] for to print random names in snack2

--- src/org/experis/snack2/RandomName.java
+++ src/org/experis/snack2/RandomName.java
@@ -5,12 +5,16 @@ import java.util.Random;
 public class RandomName {
     public static void main(String[] args) {
         String[] names = {"Antonio", "Alessandro", "Luca", "Giuseppe", "Roberto", "Marco"};
-        String[] surnames = {"Cassno", "Del Piero", "Toni", "Rossi", "Baggio", "Tardelli"};
+        String[] surnames = {"Cassano", "Del Piero", "Toni", "Rossi", "Baggio", "Tardelli"};
+        int fakeGuestList = 20;
 
         Random randomGenerator = new Random();
-        int randomName = randomGenerator.nextInt(names.length);
-        int randomSurname = randomGenerator.nextInt(surnames.length);
+        for (int i = 0; i < fakeGuestList; i++) {
+            int randomName = randomGenerator.nextInt(names.length);
+            int randomSurname = randomGenerator.nextInt(surnames.length);
 
-        System.out.println(names[randomName] + " " + surnames[randomSurname]);
+            System.out.println(names[randomName] + " " + surnames[randomSurname]);
+        }
+
     }
 }
